8d6b619e52b83f84b1d29f52742c550f7f7f0fb6
fladeos <fladeos.corp@gmail.com>
2014-05-08 17:35:06 +0700

    Berhasil Tampilkan datagrid

--- fladeos_mark2(simple_pos_sql_server)/Form1.vb
+++ fladeos_mark2(simple_pos_sql_server)/Form1.vb
@@ -1,4 +1,5 @@
-﻿Public Class Form1
+﻿Imports System.Data.SqlClient
+Public Class Form1
 
     Dim itemName, quantity, price, count As String
 
@@ -20,6 +21,7 @@
                 Dim myQuery As String = "insert into dbo.tb_transaksi values ('" & itemName & "', '" & quantity & "', '" & price & "', '" & count & "', '" & "')"
                 DB.Koneksi.cmd = New SqlClient.SqlCommand(myQuery, DB.Koneksi.conn)
                 DB.Koneksi.cmd.ExecuteNonQuery()
+
                 MessageBox.Show("Berhasil insert")
             Catch ex As Exception
                 MessageBox.Show(ex.ToString)
@@ -29,6 +31,37 @@
     End Sub
 
     Private Sub Form1_Load(sender As Object, e As EventArgs) Handles MyBase.Load
-        DB.Koneksi.ambilkoneksi()
+        DB.Koneksi.Ambilkoneksi()
+        FillDataGrid()
     End Sub
+
+
+    Public Sub FillDataGrid()
+        ''Buat query SQL untuk menampilkan
+        'Dim myQuery As String = "Select * from dbo.tb_transaksi"
+        ''Buat sebuah dataAdapter
+        'Dim adapter As New SqlDataAdapter(myQuery, DB.Koneksi.conn)
+        ''Buat sebuah dataset
+        'Dim myDataSet As New DataSet()
+        ''Isi dataSet dengan dataAdapter
+        'adapter.Fill(myDataSet, "dbo.tb_transaksi")
+        ''Isi dataSource
+        'dtg1.DataSource = myDataSet
+
+        DB.Koneksi.Ambilkoneksi()
+        Dim data As New DataSet
+        Dim tabel As String = "select * from dbo.tb_transaksi"
+        Dim adapter As New SqlDataAdapter(tabel, DB.Koneksi.conn)
+        'Dim gridview As New MySql.Data.MySqlClient.MySqlDataAdapter(tabel, Database)
+        adapter.Fill(data, "dbo.tb_transaksi")
+        Dim DataGridView As New DataView(data.Tables("dbo.tb_transaksi"))
+        dtg1.DataSource = DataGridView
+        'Me.gridAdd.Columns(0).HeaderText = "ID Item"
+        'Me.gridAdd.Columns(1).HeaderText = "Nama Item"
+        'Me.gridAdd.Columns(2).HeaderText = "Harga Satuan"
+        'Me.btnEditAdd.Enabled = False
+        'Me.btnHapusAdd.Enabled = False
+
+    End Sub
+
 End Class
---
+++ fladeos_mark2(simple_pos_sql_server)/Koneksi.vb
@@ -0,0 +1,31 @@
+﻿Imports System.Data.SqlClient
+Namespace DB
+    Module Koneksi
+        Public conn As SqlConnection
+        Public cmd As SqlCommand
+        Public ds As DataSet
+        'Public adapter As SqlDataAdapter
+        Public rd As SqlDataReader
+        Public lokasidb As String
+
+        Public Sub Ambilkoneksi()
+            lokasidb = "Data Source=ELIZABETH\SQLEXPRESS;Initial Catalog=fladeos_mark2;Persist Security Info=True;User ID=sa; password=root"
+            conn = New SqlConnection(lokasidb)
+            Try
+                If conn.State = ConnectionState.Closed Then
+                    conn.Open()
+                End If
+            Catch ex As Exception
+                MessageBox.Show(Err.Description, "Results", MessageBoxButtons.OK, MessageBoxIcon.Error, MessageBoxDefaultButton.Button1)
+            End Try
+
+        End Sub
+    End Module
+
+    Module Database
+
+        
+
+    End Module
+
+End Namespace
